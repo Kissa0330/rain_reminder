eaf1bba6982d2d2cc9976855830ebe4473c4fc22
らいか <raika4789@gmail.com>
2022-06-12 13:08:32 +0900

    webpackでcssを読み込める様に

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-// import './css/style.css'
+// import path from "path";
+
+// const css_path = path.resolve('./css/styles.css');;
+import './css/styles.css'
 // const watch_location = require("./watch_loacation");
 // watch_location();
 console.log("test");
