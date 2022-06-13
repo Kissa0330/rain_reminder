2114a9c0ac0ae84d1a1bfdddf6cb00d2a389c94d
らいか <raika4789@gmail.com>
2022-06-13 23:23:02 +0900

    delete smtp

--- src/js/send_mail.js
+++ src/js/send_mail.js
@@ -1,18 +1,5 @@
-import Email from "./smtp"
-
 function send_mail()
 {
-	Email.send({
-		Host : "smtp.yourisp.com",
-		Username : "username",
-		Password : "password",
-		To : 'them@website.com',
-		From : "you@isp.com",
-		Subject : "This is the subject",
-		Body : "And this is the body"
-	}).then(
-	  message => alert(message)
-	);
 }
 
 export default send_mail;
