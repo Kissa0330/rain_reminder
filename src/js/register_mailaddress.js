fcca3dd70123a272be43580462a0617c8cc98446
らいか <raika4789@gmail.com>
2022-06-14 20:11:16 +0900

    sytleの調整

--- src/js/register_mailaddress.js
+++ src/js/register_mailaddress.js
@@ -1,15 +1,10 @@
-import send_email from "./send_email";
-
 function register_mailaddress() {
   let email = document.getElementById("mail_field").value;
   if (!email.match(/.+@.+\..+/) && email) alert("メールアドレスの形式が間違っています\nメールアドレスをご確認ください");
   else if (email.match(/.+@.+\..+/)) {
     console.log("メールアドレスを保存しました");
     localStorage.setItem("mail", email);
-    if( document.getElementById("notice").innerHTML == "傘が必要です！！")
-    {
-      send_email();
-    }
+    document.getElementById("mail_field").value = "";
   }
 }
 
